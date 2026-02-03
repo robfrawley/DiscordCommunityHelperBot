d6cbe88343c7390b6756939dc7b2de79aa67f057
Rob Frawley 2nd <rmf@src.run>
2026-02-03 14:41:58 -0500

    upp the visibleh emoji generation to a max of 100
    Signed-off-by: Rob Frawley 2nd <rmf@src.run>

--- bot/cogs/peaky_tools/peaky_tools_message_listener.py
+++ bot/cogs/peaky_tools/peaky_tools_message_listener.py
@@ -65,7 +65,7 @@ class PeakyToolsMessageListener(commands.Cog):
         )
 
         match = BLEH_REGEX.match((message.content or "").lower())
-        count = min(50, int(match.group(1))) if match else None
+        count = min(100, int(match.group(1))) if match else None
         sends = ""
 
         for _ in range(count or 0):
